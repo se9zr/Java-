1afa31471dc63896a7b33b0657f648d32362254f
se9zr <157512511+se9zr@users.noreply.github.com>
2024-01-31 14:08:56 +0300

    Add files via upload

---
+++ WhileLoopTraining.java
@@ -0,0 +1,18 @@
+
+
+
+public class WhileLoopTraining 
+{
+    public static void main(String [] args)
+    {
+        int i = 1;
+        
+        while (i < 11)
+        {
+            System.out.println(i + " Welocom to Java!");
+            i++;
+        }
+                
+         
+    }
+}
